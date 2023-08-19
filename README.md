88c5c41e490d39d124b1c1dec66d05d0441a6c2a
zummon <zummon.space@gmail.com>
2023-08-19 07:54:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+planing to make
+
+Something breaks, needs upgrade. Let me know.
+
+### Dev tools
+
+- [MATERIAL-UI](https://material-ui.com/)
+- [Google fonts](https://fonts.google.com/)
+- [Google maps](https://www.google.com/maps)
+- [unDraw](https://undraw.co/)
+- [Fake Person](https://www.fakepersongenerator.com/)
+- [Fake Address](https://www.fakeaddressgenerator.com/)
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import translate from './translate.json';
+
+const darkmode = ref(false);
+const lang = ref('en');
+</script>
+
+<template>
+	<v-container>
+		<div class="text-center">Typography</div>
+
+		{{ lang }}
+		{{ translate[lang] }}
+
+		<v-row>
+			<v-col> title btn </v-col>
+			<v-col>
+				<v-switch
+					label="Switch"
+					color="secondary"
+					v-model="darkmode"
+				></v-switch>
+				<v-btn variant="text">
+					{{ translate[lang]['change-language'] }}
+					<v-menu activator="parent">
+						<v-list>
+							<v-list-item
+								v-for="(lng, index) in translate"
+								:key="`lang-${index}`"
+								:value="lng"
+							>
+								<v-list-item-title>{{ translate[lng].name }}</v-list-item-title>
+							</v-list-item>
+						</v-list>
+					</v-menu>
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<style scoped></style>
