f9002a33fb5a35fcb4a74c51f830c4b41cebf0b7
zummon <zummon.space@gmail.com>
2023-08-30 00:24:27 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 import translate from './translate.json';
 
 const darkmode = ref(false);
 const lang = ref('en');
+
+const translation = computed(() => translate[lang.value].translation);
+
+watch(darkmode,() => {
+	
+})
 </script>
 
 <template>
 	<v-container>
-		<div class="text-center">Typography</div>
-
-		{{ lang }}
-		{{ translate[lang] }}
+		<div class="text-center">{{ translation['translate-by'] }}</div>
 
 		<v-row>
-			<v-col> title btn </v-col>
+			<v-col>
+				<p class="text-h4">{{ translation['headline'] }}</p>
+				<v-btn
+					variant="text"
+					class=""
+					href="https://zummon.page/"
+					target="_blank"
+				>
+					{{ translation['sub-headline'] }}
+				</v-btn>
+			</v-col>
 			<v-col>
 				<v-switch
-					label="Switch"
+					:label="translation['toggle-dark-mode']"
 					color="secondary"
 					v-model="darkmode"
 				></v-switch>
 				<v-btn variant="text">
-					{{ translate[lang]['change-language'] }}
+					{{ translation['change-language'] }}
 					<v-menu activator="parent">
 						<v-list>
 							<v-list-item
@@ -39,5 +52,3 @@ const lang = ref('en');
 		</v-row>
 	</v-container>
 </template>
-
-<style scoped></style>
